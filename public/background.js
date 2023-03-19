6830911c9c843cd71642932e85b18e85cf02b1be
Sokol Paja <kolpaja28@gmail.com>
2023-03-19 20:33:40 +0100

    setup extension with option page

---
+++ public/background.js
@@ -0,0 +1,40 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.runtime.setUninstallURL('https://daily-dev-tips.com');
+
+  if (details.reason === 'install') {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icons/icon-48.png',
+      title: 'Hi there 👋',
+      message: 'Welcome to the best extensions you ever installed',
+      buttons: [{ title: 'Thanks 😅️' }],
+      priority: 0,
+    });
+  }
+
+  if (details.reason === 'update') {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icons/icon-48.png',
+      title: 'Thank you',
+      message: 'For updating this extensions',
+      buttons: [{ title: 'Cool' }],
+      priority: 0,
+    });
+  }
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'motivation_alarm') {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icons/icon-48.png',
+      title: 'Hi there 👋',
+      message: 'Just a reminder that you rock!',
+      buttons: [{ title: 'I know ☺️' }],
+      priority: 0,
+    });
+  }
+});
---
+++ src/options/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {createRoot} from 'react-dom'
+import Options from './Options'
+
+const options = document.getElementById("options");
+
+ReactDOM.render(<Options />, options);
